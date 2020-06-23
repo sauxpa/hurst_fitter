5b4a044e3b43c8d6372c78aaa28aa5eb7e6e64e9
sauxpa <46052865+sauxpa@users.noreply.github.com>
2020-06-23 11:34:28 +0200

    from Quant

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hurst_fitter',
+    version='1.1.1',
+    author="Patrick Saux",
+    author_email="patrick.jr.saux@gmail.com",
+    description="Library to estimate Hurst index of time series.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sauxpa/hurst_fitter",
+    install_requires=['numpy', 'scipy', 'pandas', 'statsmodels'],
+    python_requires='>=3.6',
+)
